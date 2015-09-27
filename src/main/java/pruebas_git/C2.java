7d150900ca3e1c8c07927bbd643ac9360c303688
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2015-09-27 11:41:23 +0200

    Modificada C2

--- src/main/java/pruebas_git/C2.java
+++ src/main/java/pruebas_git/C2.java
@@ -3,11 +3,28 @@ package pruebas_git;
 public class C2 {
 
     private int denominador;
+
     private int numerador;
-    
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
+
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
     @Override
     public String toString() {
         return "C2 [denominador=" + denominador + ", numerador=" + numerador + "]";
     }
-    
+
 }
