bd53494a4bd30cc4578af8d1e4d7c0a536af4462
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2015-09-27 11:40:15 +0200

    Modificada C2

--- src/main/java/pruebas_git/C2.java
+++ src/main/java/pruebas_git/C2.java
@@ -4,4 +4,10 @@ public class C2 {
 
     private int denominador;
     private int numerador;
+    
+    @Override
+    public String toString() {
+        return "C2 [denominador=" + denominador + ", numerador=" + numerador + "]";
+    }
+    
 }
